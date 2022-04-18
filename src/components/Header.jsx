e523edc23971c7d13d5a8495c0fa1633537b36cc
Musteba Erkan GUNAY <merkang91@gmail.com>
2022-04-18 03:11:10 +0300

    Update Commit
    - Images added
    
    - Begining of header section.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Logo from './img/logo.png'
 
 const Header = () => {
   return (
-    <div className='fixed z-50 w-screen bg-slate-500 p-6 px-16'></div>
+    <header className='fixed z-50 w-screen p-6 px-16'>
+      {/* Desktop & Tablets*/}
+      <div className='hidden md:flex w-full h-full'>
+          <div className="flex items-center gap-2" >
+              <img src={Logo} alt="Logo" className='w-10 object-cover'/>
+              <p className="text-headingColor text-x1 font-bold">CHELIVERY</p>
+          </div>
+
+          <ul className='flex items-center gap-8 ml-auto'>
+              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
+              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
+              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
+              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Service</li>
+          </ul>
+      </div>
+
+      {/* Mobile */}
+      <div className='flex md:hidden h-full w-full'>
+
+      </div>
+    </header>
   )
 }
 
